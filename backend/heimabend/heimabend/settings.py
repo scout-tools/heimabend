043c76f1d79e5da0140ed916c54c82fbbf44240e
Robert Bagdahn <r.bagdahn@forsit.de>
2019-12-25 22:48:27 +0100

    update databse

--- backend/heimabend/heimabend/settings.py
+++ backend/heimabend/heimabend/settings.py
@@ -81,9 +81,17 @@ WSGI_APPLICATION = 'heimabend.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'heimabend',
+        'USER': 'heimabend',
+        'PASSWORD': 'Kidy45$0',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
