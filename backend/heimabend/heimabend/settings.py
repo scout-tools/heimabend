35d60728894d77edc4220ab5292a54fa6e19467c
Robert Bagdahn <r.bagdahn@forsit.de>
2019-12-25 23:58:21 +0100

    added urls  to whitelist

--- backend/heimabend/heimabend/settings.py
+++ backend/heimabend/heimabend/settings.py
@@ -135,9 +135,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CORS_ORIGIN_WHITELIST = [
+    'https://heimabend.hratuga.de/'
     "https://hratuga.de",
     "http://localhost:8080",
     "http://127.0.0.1:8080"
+    "https://localhost:8080",
+    "https://127.0.0.1:8080"
 ]
 CORS_ORIGIN_REGEX_WHITELIST = [
     'http://localhost:8080',
