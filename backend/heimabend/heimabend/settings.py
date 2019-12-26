4cd07650ce82daaddced786f9d6111e97a6635f2
Robert Bagdahn <r.bagdahn@forsit.de>
2019-12-26 09:22:53 +0100

    CORS_ORIGIN_ALLOW_ALL = True

--- backend/heimabend/heimabend/settings.py
+++ backend/heimabend/heimabend/settings.py
@@ -146,6 +146,8 @@ CORS_ORIGIN_REGEX_WHITELIST = [
     'http://localhost:8080',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
