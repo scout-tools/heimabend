b31b998e3067ba5cd418299c230c4505b24a0746
Philip Hagemann <philip.hagemann@tum.de>
2020-03-29 00:05:01 +0100

    added like score calculation

--- backend/heimabend/basic/serializers.py
+++ backend/heimabend/basic/serializers.py
@@ -2,6 +2,8 @@
 import timeit
 from rest_framework import serializers
 from .models import Tag, Event, Message, Like
+from django.db.models import Sum
+from django.core.cache import cache
 
 
 class TagSerializer(serializers.HyperlinkedModelSerializer):
@@ -17,17 +19,29 @@ class TagSerializer(serializers.HyperlinkedModelSerializer):
             'color')
 
     def get_tag_count(self, obj):
-        return Event.objects.filter(tags__name__contains=obj).distinct().count()
+        return Event.objects.filter(tags__id=obj.id).distinct().count()
 
 
 class LikeSerializer(serializers.HyperlinkedModelSerializer):
+    current_median = serializers.SerializerMethodField()
+
     class Meta:
         model = Like
         fields = (
             'eventId',
             'opinionTypeId',
-            'like_created'
+            'like_created',
+            'current_median'
         )
+
+    def get_current_median(self, obj):
+        global median
+        query = Like.objects.values("eventId__id").annotate(sum=Sum('opinionTypeId')).order_by('sum')
+        median = query[int(query.count() / 2)]['sum']
+        return median
+
+
+median = 0
 
 
 class EventSerializer(serializers.HyperlinkedModelSerializer):
@@ -60,7 +74,24 @@ class EventSerializer(serializers.HyperlinkedModelSerializer):
             'like_score')
 
     def get_like_score(self, obj):
-        return 3
+        global median
+        score_id = 'like_score_' + str(obj.id)
+        likescore = cache.get(score_id)
+        if likescore is not None:
+            return likescore
+        else:
+            query = Like.objects.filter(eventId=obj.id).all().aggregate(sum=Sum('opinionTypeId'))
+            likes = query['sum']
+            border = median * 0.1
+            if likes > median + border:
+                cache.set(score_id, 3, timeout=20)
+                return 3
+            elif likes < median - border:
+                cache.set(score_id, 1, timeout=20)
+                return 1
+            else:
+                cache.set(score_id, 2, timeout=20)
+                return 2
 
 
 class MessageSerializer(serializers.HyperlinkedModelSerializer):
