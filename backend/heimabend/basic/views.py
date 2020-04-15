f35f6849c7ac1edfab734165ffab9c8608e92c7c
Philip Hagemann <philip.hagemann@tum.de>
2020-04-15 15:25:04 +0200

    Added serverside filtering of events of all possible combinations including sorting and searching

--- backend/heimabend/basic/views.py
+++ backend/heimabend/basic/views.py
@@ -1,10 +1,11 @@
 # views.py
-from rest_framework import pagination, viewsets, mixins, generics
+from rest_framework import pagination, viewsets, mixins, generics, filters
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework_tracking.mixins import LoggingMixin
 from .serializers import TagSerializer, EventSerializer, MessageSerializer, LikeSerializer, HighscoreSerializer
 from .models import Tag, Event, Message, Like
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import FilterSet, BooleanFilter, OrderingFilter, ModelMultipleChoiceFilter, NumberFilter
 
 
 class TagViewSet(LoggingMixin, viewsets.ModelViewSet):
@@ -17,17 +18,56 @@ class EventPagination(pagination.PageNumberPagination):
     page_size = 3  # the no. of company objects you want to send in one go
 
 
+class EventFilter(FilterSet):
+    isPossibleDigital = BooleanFilter(field_name='isPossibleDigital')
+    isPossibleAlone = BooleanFilter(field_name='isPossibleAlone')
+    isPrepairationNeeded = BooleanFilter(field_name='isPrepairationNeeded')
+    isActive = BooleanFilter(field_name='isActive')
+    withoutCosts = BooleanFilter(method='get_CostRating', field_name='costsRating')
+    sorter = OrderingFilter(fields=(
+        ('createdAt', 'newest'),
+        ('title', 'rating'),
+        ('?', 'random'),
+        ('title', 'alpha')
+    ))
+
+    filterTags = ModelMultipleChoiceFilter(field_name='tags__id',
+                                           to_field_name='id',
+                                           queryset=Tag.objects.all(),
+                                           lookup_expr='contains', )
+    isLvlOne = BooleanFilter(field_name='isLvlOne')
+    isLvlTwo = BooleanFilter(field_name='isLvlTwo')
+    isLvlThree = BooleanFilter(field_name='isLvlThree')
+
+    class Meta:
+        model = Event
+        fields = [  # 'isPossibleInside',
+            # 'isPossibleOutside',
+            'isPossibleAlone',
+            'isPossibleDigital',
+            'isPrepairationNeeded',
+            'isActive',
+            'withoutCosts',
+            'sorter',
+            'filterTags',
+            'isLvlOne',
+            'isLvlTwo',
+            'isLvlThree'
+        ]
+
+    def get_CostRating(self, queryset, field_name, value):
+        if value:
+            return queryset.filter(costsRating=0)
+        return queryset
+
+
 class EventViewSet(LoggingMixin, viewsets.ModelViewSet):
-    queryset = Event.objects.all().order_by('title')
+    queryset = Event.objects.all()
     serializer_class = EventSerializer
     pagination_class = EventPagination
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['isPossibleOutside',
-                        'isPossibleAlone',
-                        'isPossibleDigital',
-                        'isPrepairationNeeded',
-                        'isActive',
-                        ]
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    filterset_class = EventFilter
+    search_fields = ['^title', 'description', 'material']
 
 
 class MessageViewSet(LoggingMixin, viewsets.ModelViewSet):
--- backend/heimabend/heimabend/settings.py
+++ backend/heimabend/heimabend/settings.py
@@ -166,7 +166,8 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework_simplejwt.authentication.JWTAuthentication', ],
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend',
+                                'rest_framework.filters.SearchFilter'],
 }
 
 SIMPLE_JWT = {
