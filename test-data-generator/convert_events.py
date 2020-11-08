73e5cbe59645495996ff42bc1a587422faaabf9e
RoB - Robert Bagdahn <rob@ROB-MACBOOK.local>
2020-11-08 18:10:54 +0100

    added createdBy to migration script

--- test-data-generator/convert_events.py
+++ test-data-generator/convert_events.py
@@ -19,6 +19,7 @@ for i, event in enumerate(events, start=1):
     result[i-1]['fields']['id'] = event['id']
     result[i-1]['fields']['title'] = event['title']
     result[i-1]['fields']['createdAt'] = event['createdAt']
+    result[i-1]['fields']['createdBy'] = event['createdBy']
     result[i-1]['fields']['material'] = event['material']
     result[i-1]['fields']['costsRating'] = event['costsRating']
     result[i-1]['fields']['description'] = event['description']
