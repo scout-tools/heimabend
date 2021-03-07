592c406463bcf0a83b5d333018dec62af435e556
RoB - Robert Bagdahn <rob@ROB-MACBOOK.local>
2021-03-07 01:54:17 +0100

    fixed eslint

--- frontend/src/views/scoring/Main.vue
+++ frontend/src/views/scoring/Main.vue
@@ -2,35 +2,31 @@
   <v-form>
     <v-container fluid>
       <v-row align="center" justify="center">
-          <h1>Ich bin Inspi</h1>
+        <h1>Ich bin Inspi</h1>
       </v-row>
       <v-row align="center" justify="center">
-        <v-img
-          :src="require('@/assets/inspi.png')"
-          max-width="200"
-        />
+        <v-img :src="require('@/assets/inspi.png')" max-width="200" />
       </v-row>
       <v-row align="center" justify="center">
         <v-col cols="12">
           <p>
-            <b>Deine Meinung</b> ist meine Leibspeise. <br>
-            Helfe mir bitte statt zu werden.<br>
-            <br>
-            Damit ich dir passendsten Inspirationen bieten kann,
-            brauche ich Futter. Füttere sie mit so vielen Ideen und 
-            Einschätzungen von dir wie möglich, damit ich allen 
-            Pfadfindern die besten Inspirationen bieten kann.<br>
+            <b>Deine Meinung</b> ist meine Leibspeise. <br />
+            Helfe mir bitte statt zu werden.<br />
+            <br />
+            Damit ich dir passendsten Inspirationen bieten kann, brauche ich
+            Futter. Füttere sie mit so vielen Ideen und Einschätzungen von dir
+            wie möglich, damit ich allen Pfadfindern die besten Inspirationen
+            bieten kann.<br />
           </p>
         </v-col>
       </v-row>
       <v-row align="center" justify="center">
         <v-col cols="12">
-          <p class="text-center"> <b>Wie alt bist du?</b></p>
+          <p class="text-center"><b>Wie alt bist du?</b></p>
         </v-col>
         <v-btn-toggle color="primary" v-model="data.age_level" mandatory>
           <v-btn> - </v-btn>
-          <v-btn> 7-10 </v-btn>
-          <v-btn> 10-13 </v-btn>
+          <v-btn> 6-13 </v-btn>
           <v-btn> 14-18 </v-btn>
           <v-btn> 19-30 </v-btn>
           <v-btn> 30-60 </v-btn>
@@ -46,7 +42,6 @@
         <v-btn-toggle color="primary" v-model="data.group_leader" mandatory>
           <v-btn> Nein </v-btn>
           <v-btn> Ja </v-btn>
-
         </v-btn-toggle>
       </v-row>
 
@@ -76,10 +71,10 @@
       <v-divider class="my-5"></v-divider>
       <v-row align="center" justify="center">
         <v-col cols="12">
-            <v-btn color="primary" @click="createExperiment">
-              <v-icon left>mdi-flash</v-icon>
-              Loslegen!
-            </v-btn>
+          <v-btn color="primary" @click="createExperiment">
+            <v-icon left>mdi-flash</v-icon>
+            Loslegen!
+          </v-btn>
         </v-col>
       </v-row>
     </v-container>
